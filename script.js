07bc5104a6920bb6fb99b7fd02ca71403b46b5bd
Lyes2003 <chikhlyes55@gmail.com>
2025-11-08 19:04:43 -0500

    ajout des fichiers de base

---
+++ script.js
@@ -0,0 +1,8 @@
+var valeur = 42;
+var valeur2 = 100;
+
+var test = function sommedeseuxnombres(a, b) {
+    return a + b;
+};
+
+console.log( "la somme des deux nombres est : " + test(valeur, valeur2));
